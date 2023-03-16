16c5e950127c408a2034d88ee38b8548c3515607
Noah Ruben <nruben5684@gmail.com>
2023-03-16 17:55:16 +0000

    form div

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -44,48 +44,58 @@ export default function NewPlaceForm() {
       <h3>Add a new place</h3>
       <div>
         <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
       </div>
       <div>
         <label htmlFor="neighborhood">Neighborhood</label>
-        <input
-          type="text"
-          placeholder="Neighborhood"
-          value={neighborhood}
-          onChange={(e) => setNeighborhood(e.target.value)}
-        />
+        <div>
+          <input
+            type="text"
+            placeholder="Neighborhood"
+            value={neighborhood}
+            onChange={(e) => setNeighborhood(e.target.value)}
+          />
+        </div>
       </div>
       <div>
         <label htmlFor="cuisine">Cuisine</label>
-        <input
-          type="text"
-          placeholder="Cuisine"
-          value={cuisine}
-          onChange={(e) => setCuisine(e.target.value)}
-        />
+        <div>
+          <input
+            type="text"
+            placeholder="Cuisine"
+            value={cuisine}
+            onChange={(e) => setCuisine(e.target.value)}
+          />
+        </div>
       </div>
       <div>
         <label htmlFor="rating">Rating</label>
-        <input
-          type="number"
-          placeholder="Rating"
-          value={rating}
-          onChange={(e) => setRating(e.target.valueAsNumber)}
-        />
+        <div>
+          <input
+            type="number"
+            placeholder="Rating"
+            value={rating}
+            onChange={(e) => setRating(e.target.valueAsNumber)}
+          />
+        </div>
       </div>
       <div>
         <label htmlFor="price">Price</label>
-        <input
-          type="text"
-          placeholder="Price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
+        <div>
+          <input
+            type="number"
+            placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
       </div>
       <button type="submit">Add Place</button>
     </form>
