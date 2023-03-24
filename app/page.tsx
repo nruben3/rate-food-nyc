71873d2dc8bf32c5edd6943ea6d3339905fc4e8d
Noah Ruben <nruben5684@gmail.com>
2023-03-24 14:21:04 +0000

    new

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,7 @@ async function getNeighborhoods() {
     }
   )
   const data: any = await response.json()
-  const results: any[] = data.results
-  for (const neighborhood of results) {
+  for (const neighborhood of data.results) {
     if (!neighborhoods[neighborhood.borough]) {
       neighborhoods[neighborhood.borough] = []
     }
