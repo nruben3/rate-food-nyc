1fe5de946b48393480419f1d54250ea79cea255d
Noah Ruben <nruben5684@gmail.com>
2023-03-24 15:05:30 +0000

    error

--- app/components/PlacesList.tsx
+++ app/components/PlacesList.tsx
@@ -4,26 +4,25 @@ import Place, { IPlace } from "@/lib/models/place"
 import PlaceCard from "./PlaceCard"
 
 async function getPlaces() {
-  try {
-    await connect(process.env.MONGODB_URI || "")
-  } catch (e: any) {
-    return e
-  }
+  await connect(process.env.MONGODB_URI || "")
   return JSON.stringify(await Place.find())
 }
 
 export default async function PlacesList() {
-  const response = await getPlaces()
-  const places: IPlace[] = JSON.parse(response)
-
-  return (
-    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 m-2 w-full">
-      <h3 className="block text-gray-700 text-xl font-bold mb-2">Places</h3>
-      <div className="grid gap-4 grid-cols-5">
-        {places.map((place, index) => (
-          <PlaceCard place={place} key={index} />
-        ))}
+  try {
+    const response = await getPlaces()
+    const places: IPlace[] = JSON.parse(response)
+    return (
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 m-2 w-full">
+        <h3 className="block text-gray-700 text-xl font-bold mb-2">Places</h3>
+        <div className="grid gap-4 grid-cols-5">
+          {places.map((place, index) => (
+            <PlaceCard place={place} key={index} />
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
+  } catch (error: any) {
+    return <h1>Error: {error.message}</h1>
+  }
 }
